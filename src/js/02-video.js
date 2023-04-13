381005037d73e6138f397ff0a9f7ffeab4483ee9
akhsudop <gov.alexpillow@gmail.com>
2023-04-13 23:45:15 +0200

    ended 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+const MY_KEY = 'videoplayer-current-time';
+const seconds = 0;
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem(MY_KEY)))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+const play = e => {
+  localStorage.setItem(MY_KEY, JSON.stringify(e.seconds));
+};
+player.on('timeupdate', throttle(play, 1000));
