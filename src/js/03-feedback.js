a92d022089316654f7e7bafb5a827340ce00fd0b
akhsudop <gov.alexpillow@gmail.com>
2023-04-14 16:38:12 +0200

    added task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import { throttle } from 'lodash';
 
+MY_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const saveInput = e => {
+  localStorage.setItem(
+    MY_KEY,
+    JSON.stringify({
+      message: e.currentTarget.message.value,
+      email: e.currentTarget.email.value,
+    })
+  );
+};
+
+form.addEventListener('input', saveInput);
+
+const submitForm = e => {
+  e.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(MY_KEY)));
+  console.log(JSON.parse(localStorage.getItem(MY_KEY)).entries);
+  // console.log(
+  //   `message: ${JSON.parse(localStorage.getItem(MY_KEY)).message} email: ${
+  //     JSON.parse(localStorage.getItem(MY_KEY)).email
+  //   }`
+  // );
+  localStorage.clear();
+};
+
+form.addEventListener('submit', submitForm);
